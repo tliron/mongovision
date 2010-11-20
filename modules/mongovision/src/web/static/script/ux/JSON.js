45bf1b950357121986154a9739aba412930125d6
Tal Liron <tal.liron@gmail.com>
2010-11-20 22:02:58 +0000

    JSON encoding fix
    git-svn-id: https://mongo-vision.googlecode.com/svn/trunk@51 74fc5d71-c326-839a-d948-4d448537368a

--- modules/mongovision/src/web/static/script/ux/JSON.js
+++ modules/mongovision/src/web/static/script/ux/JSON.js
@@ -13,13 +13,40 @@
 // Ext.ux.JSON
 //
 // A JSON encoder that supports optional multiline indenting, HTML vs. plain text,
-// and removing curly brackets from the root object.
+// and removing curly brackets from the root object. The point is to produce
+// human-readable JSON, not necessarily the most compact JSON.
 //
 // We've been inspired by the code in Ext.util.JSON, though have diverged
-// significantly.
+// significantly. We also use some code from Douglas Crockford's json2.js.
 //
 
 Ext.namespace('Ext.ux.JSON');
+
+Ext.ux.JSON.escapable = /[\\\"\x00-\x1f\x7f-\x9f\u00ad\u0600-\u0604\u070f\u17b4\u17b5\u200c-\u200f\u2028-\u202f\u2060-\u206f\ufeff\ufff0-\uffff]/g;
+Ext.ux.JSON.meta = {
+	'\b': '\\b',
+	'\t': '\\t',
+	'\n': '\\n',
+	'\f': '\\f',
+	'\r': '\\r',
+	'"' : '\\"',
+	'\\': '\\\\'
+};
+
+Ext.ux.JSON.quote = function(string) {
+	// Yanked from Douglas Crockford's json2.js (public domain)
+	
+	// If the string contains no control characters, no quote characters, and no
+	// backslash characters, then we can safely slap some quotes around it.
+	// Otherwise we must also replace the offending characters with safe escape
+	// sequences.
+	
+	Ext.ux.JSON.escapable.lastIndex = 0;
+	return Ext.ux.JSON.escapable.test(string) ? '"' + string.replace(Ext.ux.JSON.escapable, function(a) {
+		var c = Ext.ux.JSON.meta[a];
+		return typeof c === 'string' ? c : '\\u' + ('0000' + a.charCodeAt(0).toString(16)).slice(-4);
+	}) + '"' : '"' + string + '"';
+};
 
 Ext.ux.JSON.encode = function(value, html, multiline) {
 	function toJSON(value, html, multiline, indent, depth) {
@@ -43,8 +70,7 @@ Ext.ux.JSON.encode = function(value, html, multiline) {
 				value = value.replace(/</g, '&lt;');
 				value = value.replace(/>/g, '&gt;');
 			}
-			value = value.replace(/"/g, '\\"');
-			json += '"' + value + '"';
+			json += Ext.ux.JSON.quote(value);
 		}
 		else if (typeof value == 'number') {
 			// Don't use isNumber here, since finite checks happen inside isNumber
